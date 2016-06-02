c9c764a5bafb76716da06a5046c5a37f5d240bce
Bob Whitelock <bob.whitelock1@gmail.com>
2016-06-02 22:14:21 +0100

    Rust/word-count/2

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -2,24 +2,20 @@
 use std::collections::HashMap;
 
 pub fn word_count(input: &str) -> HashMap<String, u32> {
-    let punctuation = vec![
-        '!', '"', '$', '%', '^', '&', '*', '(', ')', ':', ',', '.', '_', '-',
-        '#', '@',
-    ];
-
     let munged_input = input
         .chars()
         .map(|c| match c {
-            _ if punctuation.contains(&c) => ' ',
-            _ => c,
+            _ if c.is_alphanumeric() => c,
+            _ => ' ',
         })
-        .collect::<String>();
+        .collect::<String>()
+        .to_lowercase();
 
     let mut word_counts = HashMap::new();
 
     let words = munged_input.split_whitespace();
     for word in words {
-        let word = word.to_string().to_lowercase();
+        let word = word.to_string();
         let count = match word_counts.get(&word) {
             Some(current_count) => current_count + 1,
             None => 1,
