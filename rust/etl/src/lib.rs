94d706a37e39fd1e319115d0b30e970a0b6a6ae9
Bob Whitelock <bob.whitelock1@gmail.com>
2016-06-17 16:53:24 +0100

    Rust/etl/2

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -5,7 +5,7 @@ pub fn transform(input: &BTreeMap<i32, Vec<String>>) -> BTreeMap<String, i32> {
     let mut result = BTreeMap::new();
     for (&points, letters) in input.iter() {
         for letter in letters.iter() {
-            result.insert(letter.clone().to_lowercase(), points);
+            result.insert(letter.to_lowercase(), points);
         }
     }
     result
