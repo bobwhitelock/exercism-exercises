441ac2946fed50c5fb3bd41868d924cea9087817
Bob Whitelock <bob.whitelock1@gmail.com>
2018-02-07 23:04:36 +0000

    Go/gigasecond/2

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,9 +1,8 @@
 package gigasecond
 
-import "math"
 import "time"
 
 func AddGigasecond(t time.Time) time.Time {
-	gigasecond := time.Duration(math.Pow(10, 9)) * time.Second
+	gigasecond := time.Duration(1e9 * time.Second)
 	return t.Add(gigasecond)
 }
