1149d17b9eef3f64f2ee923dc64fd602f5e2f864
Bob Whitelock <bob.whitelock1@gmail.com>
2018-02-07 23:42:48 +0000

    Go/bob/2

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -5,32 +5,32 @@ import "regexp"
 
 func Hey(remark string) string {
 	remark_ := strings.TrimSpace(remark)
-	is_question := IsQuestion(remark_)
-	is_upper := IsUpper(remark_)
+	question := isQuestion(remark_)
+	upper := isUpper(remark_)
 
-	if IsBlank(remark_) {
+	if isBlank(remark_) {
 		return "Fine. Be that way!"
-	} else if is_question && is_upper {
+	} else if question && upper {
 		return "Calm down, I know what I'm doing!"
-	} else if is_question {
+	} else if question {
 		return "Sure."
-	} else if is_upper {
+	} else if upper {
 		return "Whoa, chill out!"
 	} else {
 		return "Whatever."
 	}
 }
 
-func IsBlank(str string) bool {
+func isBlank(str string) bool {
 	return str == ""
 }
 
-func IsUpper(str string) bool {
+func isUpper(str string) bool {
 	is_lower := strings.ToLower(str) == str
 	return !is_lower && strings.ToUpper(str) == str
 }
 
-func IsQuestion(str string) bool {
+func isQuestion(str string) bool {
 	question_regex := regexp.MustCompile("\\?$")
 	return question_regex.MatchString(str)
 }
