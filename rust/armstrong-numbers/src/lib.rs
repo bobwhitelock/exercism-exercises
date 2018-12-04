ba5e905bf748deefdaf5dd28ea1f45c5a5cb40b4
Bob Whitelock <bob.whitelock1@gmail.com>
2018-12-04 00:59:09 +0000

    Rust/armstrong-numbers/1

---
+++ rust/armstrong-numbers/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn is_armstrong_number(num: u32) -> bool {
+    let digits = digits(num);
+    let digits_length = digits.len() as u32;
+    let summed_digit_powers: u32 = digits.iter().map(|d| d.pow(digits_length)).sum();
+    summed_digit_powers == num
+}
+
+fn digits(num: u32) -> Vec<u32> {
+    num.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).expect("Must be a digit"))
+        .collect()
+}
